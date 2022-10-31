4828345f2d6c444352d54889177522c536aaf840
Emmett Naughton <enaughton5@gmail.com>
2022-10-31 15:56:38 -0400

    first commit

---
+++ components/JobList.vue
@@ -0,0 +1,69 @@
+<script>
+import { EditIcon, Trash2Icon } from 'vue-feather-icons'
+
+export default {
+  components: {
+    EditIcon,
+    Trash2Icon,
+  },
+  props: {
+    job: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+
+  methods: {
+    onEdit(job) {
+      alert(`editing ${job.title}`)
+    },
+    onDelete(job) {
+      alert(`Deleting ${job.title}`)
+    },
+  },
+}
+</script>
+
+
+<template>
+  <li
+    class="
+      p-4
+      mb-3
+      flex
+      justify-between
+      items-center
+      bg-white
+      shadow
+      rounded-lg
+      cursor-move
+    "
+  >
+    <div class="flex items-center">
+      <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">
+        {{ job.title }}
+      </p>
+    </div>
+    <div class="flex">
+      <button
+        aria-label="Edit user"
+        class="
+          p-1
+          focus:outline-none focus:shadow-outline
+          text-teal-500
+          hover:text-teal-600
+        "
+        @click="onEdit(job)"
+      >
+        <EditIcon />
+      </button>
+      <button
+        aria-label="Delete user"
+        class="p-1 focus:outline-none focus:shadow-outline hover:text-red-600"
+        @click="onDelete(job)"
+      >
+        <Trash2Icon />
+      </button>
+    </div>
+  </li>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="bg-gradient-to-br from-green-600">
+    <Header />
+    <div
+      id="app"
+      class="
+        min-h-screen
+        w-screen
+        flex flex-col
+        pt-20
+        justify-center
+        items-center
+        md:items-start md:flex-row
+      "
+    >
+      <div class="w-full max-w-md bg-green text-center px-3">
+        <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">
+          Active Jobs
+        </p>
+
+        <Draggable
+          tag="ul"
+          class="w-full max-w-md"
+          ghost-class="moving-card"
+          group="all-jobs"
+          :list="users"
+          :animation="200"
+        >
+          <JobList
+            v-for="job in jobs"
+            :job="job"
+            :key="job.title"
+            @on-edit="onEdit"
+            @on-delete="onDelete"
+          >
+          </JobList>
+        </Draggable>
+      </div>
+      <div class="w-full max-w-md ml-12 text-center">
+        <p class="mb-2 text-gray-700 font-semibold font-sans tracking-wide">
+          Completed Applications
+        </p>
+        <Draggable
+          tag="ul"
+          ghost-class="moving-card"
+          group="all-jobs"
+          :list="appliedJobs"
+          :animation="200"
+        >
+          <JobList
+            v-for="job in appliedJobs"
+            :job="job"
+            :key="job.title"
+            @on-edit="onEdit"
+            @on-delete="onDelete"
+          >
+          </JobList>
+        </Draggable>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Draggable from 'vuedraggable'
+import jobs from '/jobs.json'
+export default {
+  components: {
+    Draggable,
+  },
+  data() {
+    return {
+      jobs: jobs
+    }
+  },
+  name: 'IndexPage',
+}
+</script>
+s
